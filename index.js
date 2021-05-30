f2126478d03ab3d477c3830d9d25b5e5a54829b1
anuragbhardwaj1303 <1914231.cse.coe@cgc.edu.in>
2021-05-31 00:39:29 +0530

    final

--- index.js
+++ index.js
@@ -1,38 +1,22 @@
 const h1 = document.querySelector("#h1");
 const h12 = document.querySelectorAll("#h2")[1];
+const cataegory = document.querySelector("#type");
 
-const callApi = (para) => {
-  if (para === 1) {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-    fetch("https://v2.jokeapi.dev/joke/Any?type=twopart", requestOptions)
-      .then((resp) => {
-        return resp.json();
-      })
-      .then((data) => {
-        h1.innerHTML = `setup: ${data.setup}`;
-        h2.innerHTML = `Delivery: ${data.delivery}`;
-      });
-  } else if (para === 2) {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-    fetch("https://v2.jokeapi.dev/joke/Any?type=single", requestOptions)
-      .then((resp) => {
-        return resp.json();
-      })
-      .then((data) => {
-        console.log(data.joke);
-      });
-  }
+const requestOptions = {
+  method: "GET",
+  redirect: "follow",
 };
-
-let btn1 = document
-  .querySelector("#btn1")
-  .addEventListener("fetch", callApi(1));
-let btn2 = document
-  .querySelector("#btn2")
-  .addEventListener("fetch", callApi(2));
+fetch("https://v2.jokeapi.dev/joke/Any", requestOptions)
+  .then((resp) => {
+    return resp.json();
+  })
+  .then((data) => {
+    if (data.type === "twopart") {
+      cataegory.innerText = `Category:${data.category}`;
+      h1.innerHTML = `${data.setup}`;
+      h2.innerHTML = `${data.delivery}`;
+    } else {
+      cataegory.innerHTML = `Category:${data.category}`;
+      h1.innerHTML = data.joke;
+    }
+  });
